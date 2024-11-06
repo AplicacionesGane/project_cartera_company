b98a16acf36b3a70c898c422dac6da5d35b7b0ed
llOrtegall <IvanOrtega_97@hotmail.com>
2024-11-06 08:15:10 -0500

    Refactor NavBar and UserInfo components for improved styling and functionality

--- client/src/components/ui/UserInfo.tsx
+++ client/src/components/ui/UserInfo.tsx
@@ -2,14 +2,13 @@ import { useAuth } from '../../auth/AuthProvider'
 import { LOGIN_URL } from '../../utils/contanst'
 import { User } from '../../types/user'
 import { LogoutIcon } from '../icons'
-import { Button } from './Button'
 import axios from 'axios'
 
 interface Props {
   user: User
 }
 
-function UserInfo({ user }: Props) {
+function UserInfo ({ user }: Props) {
   const { setUser, setIsAuthenticated } = useAuth()
 
   const handleLogout = () => {
@@ -25,16 +24,17 @@ function UserInfo({ user }: Props) {
 
   return (
     <>
-      <h2 className='text-center font-semibold border-b mb-2 pb-1 text-lg'>Información Usuario</h2>
-      <p className='font-medium text-center'>User: {user.username}</p>
-      <article className='mb-2 flex flex-col gap-1'>
-        <p className='font-medium text-center'><span>{user.names} {user.lastnames}</span></p>
+      <h2 className='font-semibold border-b border-gray-700 text-lg'>Usuario</h2>
+      <article className=''>
+        <p className='font-medium'>User: {user.username}</p>
+        <p className='font-medium'><span>{user.names} {user.lastnames}</span></p>
         <p className='font-medium'>{user.email}</p>
       </article>
 
-      <Button onClick={handleLogout}>
+      <button className='bg-blue-700 text-white rounded-full h-10 w-10 text-xl flex items-center justify-center cursor-pointer hover:bg-blue-500 dark:hover:bg-dark-tremor-brand-faint dark:bg-dark-tremor-brand-faint'
+        onClick={handleLogout}>
         <LogoutIcon />
-      </Button>
+      </button>
     </>
   )
 }
