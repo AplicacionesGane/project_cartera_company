e1a86aee9599be49f82dd86ccd8a307f063ce068
llOrtegall <IvanOrtega_97@hotmail.com>
2024-11-12 10:49:25 -0500

    Enable logging in Sequelize connection for improved debugging

--- api/src/connections/index.ts
+++ api/src/connections/index.ts
@@ -6,7 +6,7 @@ const conection = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   port: DB_PORT,
   dialect: 'mysql',
   timezone: '-05:00', 
-  logging: false
+  logging: true
 });
 
 export { conection };
