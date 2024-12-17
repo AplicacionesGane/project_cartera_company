5ff7f4f88302cc0c57dd4b7f730f0f585f931b5a
llOrtegall <IvanOrtega_97@hotmail.com>
2024-12-17 12:13:00 -0500

    refactor InfoPersonReport component to simplify rendering logic

--- client/src/components/InfoPersonReport.tsx
+++ client/src/components/InfoPersonReport.tsx
@@ -1,7 +1,7 @@
 import { DataOracle } from '../types/Recaudo'
 import { Card, Label, Badge } from './ui' // Asegúrate de importar los componentes necesarios
 
-export const InfoPersonReport = ({ data, fecha }:{data: DataOracle[], fecha: string}) => {
+export const InfoPersonReport = ({ data, fecha }: { data: DataOracle[], fecha: string }) => {
   const firstResult = data && data.length > 0 ? data[0] : { persona: '', nombres: '', sucursal: '', nombre_comercial: '' }
 
   return (
@@ -17,14 +17,6 @@ export const InfoPersonReport = ({ data, fecha }:{data: DataOracle[], fecha: str
       <div className='col-span-3'>
         <Label>Nombres: </Label>
         <Badge variant='warning'>{firstResult.nombres || ''}</Badge>
-      </div>
-      <div className='col-span-1'>
-        <Label>Sucursal: </Label>
-        <Badge variant='warning'>{firstResult.sucursal || ''}</Badge>
-      </div>
-      <div className='col-span-3'>
-        <Label>Nombre Sucursal: </Label>
-        <Badge variant='warning'>{firstResult.nombre_comercial || ''}</Badge>
       </div>
     </Card>
   )
