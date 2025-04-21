30c88696bacaca820d3486198eb8212c8e561aad
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-21 11:13:27 -0500

    change auth provider remove initial user

--- client/src/auth/AuthProvider.tsx
+++ client/src/auth/AuthProvider.tsx
@@ -1,33 +1,23 @@
 import { createContext, useContext, useEffect, useState } from 'react'
-import { APP_NAME, LOGIN_URL } from '../utils/contanst'
+import { LOGIN_URL } from '../utils/contanst'
 import { type User } from '../types/user'
 import axios from 'axios'
 
 interface IAuthContext {
   isAuthenticated: boolean
-  user: User
-  setUser: React.Dispatch<React.SetStateAction<User>>
+  user: User | null
+  setUser: React.Dispatch<React.SetStateAction<User | null>>
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>
 }
-
-const InitialUser: User = { username: '', email: '', names: '', lastnames: '', company: '', process: '', sub_process: '', id: '', app: '' }
 
 const AuthContext = createContext<IAuthContext | undefined>(undefined)
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }): JSX.Element => {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const [user, setUser] = useState<User>(InitialUser)
+  const [user, setUser] = useState<User | null>(null)
 
   useEffect(() => {
-    const cookie = document.cookie
-
-    if (!cookie && cookie.split('=')[0] !== APP_NAME) {
-      setIsAuthenticated(false)
-      setUser(InitialUser)
-      return
-    }
-
-    axios.get(`${LOGIN_URL}/profile`, { params: { app: APP_NAME } })
+    axios.get(`${LOGIN_URL}/profile`)
       .then(res => {
         if (res.status === 200) {
           setIsAuthenticated(true)
@@ -37,7 +27,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }): JSX.E
       .catch(error => {
         if (error.response.status === 401) {
           setIsAuthenticated(false)
-          setUser(InitialUser)
+          setUser(null)
         }
       })
   }, [isAuthenticated])
