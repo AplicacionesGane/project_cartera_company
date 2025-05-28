bec98473c4d51abbcbd1cdf53b7a95edea83d82e
Servired - Multired <aplicaciones@grupomultired.com.co>
2025-05-28 16:01:18 -0500

    fix error proxy reverse

--- client/src/utils/contanst.ts
+++ client/src/utils/contanst.ts
@@ -1,3 +1,3 @@
-export const API_URL = import.meta.env.VITE_API_URL ?? '/api'
-export const LOGIN_URL = import.meta.env.VITE_LOGIN_URL ?? '/data'
+export const API_URL = import.meta.env.VITE_API_URL ?? '/data'
+export const LOGIN_URL = import.meta.env.VITE_LOGIN_URL ?? '/api'
 export const APP_NAME = import.meta.env.VITE_APP_NAME
